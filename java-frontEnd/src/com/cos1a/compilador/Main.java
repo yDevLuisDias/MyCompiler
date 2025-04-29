7c6154919eefb372a45e2eebf62c572f25d22cce
yDevLuisDias <luiscosta.official@gmail.com>
2025-04-29 20:27:49 -0300

    Correto

--- java-frontEnd/src/com/cos1a/compilador/Main.java
+++ java-frontEnd/src/com/cos1a/compilador/Main.java
@@ -3,6 +3,5 @@ package com.cos1a.compilador;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world!");
-        System.out.println("teste branch ");
     }
 }
