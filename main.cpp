9c513b03cdabcf89532c5bebc1d2d506ad49a4e5
Basquat <joaodan.lisboa@gmail.com>
2025-05-05 19:18:26 -0300

    medo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include "COMPILER.H"
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <stdio.h>
+#include <unistd.h>
 
 using namespace std;
-string test;
+int test;
 
 
 int main()
 {
-    cin >> test;
-    cout << test << endl;
+  cout << "teste iniciando compilador" << endl;
+  cin >> test;
+  if (test == 1) {
+    for (test = 1; test <= 100; test++) {
+            fflush(stdin);
+    cout << "\r";
+        cout << test << "%";
+        fflush(stdout);
+    }
+  }
 
 
     return 0;
